6db06f729a7063384670f6d284bb03f92192ef18
CookiePixelated <ulannags@gmail.com>
2025-03-04 08:19:03 +0800

    modified and relocated

--- main.lua
+++ main.lua
@@ -1,4 +1,5 @@
-local wf = require "windfield"
+local wf = require "libraries.windfield"
+local utf8 = require "utf8"
 
 function love.load()
     -- Window properties
@@ -27,13 +28,38 @@ function love.load()
     n = -180 -- star's revolution
     ww, wh = love.graphics.getDimensions() -- window size
 
+    -- Barriers
     Ground = World:newRectangleCollider(0, wh-40, ww, 40)
     Ground:setType("static")
 
-    Ball = World:newCircleCollider(ww/2, wh/2, 20)
-    Ball:setRestitution(1)
+    Left_Wall = World:newRectangleCollider(-30, 0, 30, wh)
+    Left_Wall:setType("static")
+
+    Right_Wall = World:newRectangleCollider(ww, 0, 30, wh)
+    Right_Wall:setType("static")
+
+    Roof = World:newRectangleCollider(0, -40, ww, 30)
+    Roof:setType("static")
+
+    text = ""
+    test = false
 end
 
+function love.textinput(t)
+    text = text .. t
+end
+
+function love.keypressed(key)
+    if key == "backspace" then
+        local byteoffset = utf8.offset(text, -1)
+
+        if byteoffset then
+            text = string.sub(text, 1, byteoffset - 1)
+        end
+    end
+end
+
+-- Star point calculation
 function calculate_point(m) -- determines the points of a star
     local radius = 15
     local angle_between_points = 72
@@ -50,6 +76,20 @@ function love.update(dt)
     World:update(dt)
     local speed = 10
     n = n + 36 * dt
+
+    -- granter
+    local wish = string.lower(text)
+    test = false
+    
+    if wish == "circle" then
+        test = true
+    elseif wish == "square" then
+        test = true
+    end
+
+    if string.lower(text) == "square" then
+        test = true
+    end
 end
 
 function love.draw()
@@ -72,4 +112,10 @@ function love.draw()
     -- dirt
     love.graphics.setColor(75/250, 37.5/250, 0)
     love.graphics.rectangle("fill", 0, wh, ww, -15)
+
+    -- input
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.print(text, ww/2, wh/2)
+
+    love.graphics.print(tostring(test), 100, 100)
 end
